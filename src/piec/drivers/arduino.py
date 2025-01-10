c474698e10820c3516763caea7af64a0cf061148
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2025-01-10 14:36:35 -0500

    Update arduino.py
    updated arduino.py, but NOTE requires arduino code to be updated as well

---
+++ src/piec/drivers/arduino.py
@@ -0,0 +1,35 @@
+"""
+Set's up the arduino instrument class that all arduino instruments will inherit basic functionlity from.
+
+NOTE: SHOULD UPDATE ARDUINO AND HARDCODE NEW NUMBER. ALSO USE DIR, SINCE DIR IS ONLY 1 or 2 can use like whateer num i want then no step number is not allowed
+"""
+
+from instrument import Instrument
+
+class Arduino_Stepper(Instrument):
+    """
+    Custom Class for using an arduino as a stepper motor driver. See https://github.com/ElPsyKurisu/STFMR/tree/main/Arduino for more information
+    """
+    def step(self, num_steps, direction):
+        """
+        Steps the stepper motor 
+        args:
+            self (pyvisa.resources.asrl.not sure): Arduino
+            num_steps (str/int): Desired step size, must be an integer value
+            direction (str/int): [0,1] are the ONLY allowed values, 0 for CW 1 for CCW (not could be backwards)
+        """
+        self.write("{},{}".format(num_steps, direction)) #specially formatted string for arduino code to work. See https://github.com/ElPsyKurisu/STFMR/tree/main/Arduino for more information
+    
+    def set_zero(self):
+        """
+        Hardcoded command that sets the arduinos position tracker to zero
+        """
+        self.step(0,9)
+
+    def read_position(self):
+        """
+        Reads the current position without stepping the stepper
+        """
+        self.step(0,99)
+        return self.read()
+
