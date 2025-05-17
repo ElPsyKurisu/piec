2a9f5ac365b9010cef550156c781bb984d91d66f
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2025-05-17 18:50:16 -0400

    New Outline created

---
+++ src/piec/drivers/outline/generator.py
@@ -0,0 +1,22 @@
+"""
+This is an outline for what the generator.py file should be like.
+
+A generator is defined as an instrument that generates something (e.g. a voltage/current)
+"""
+from .instrument import Instrument
+class Generator(Instrument):
+    # Initializer / Instance attributes
+    """
+    All an instrument is required to have is an address!
+    """
+    def __init__(self, address):
+        """
+        Opens the instrument and enables communication with it. Still communication method agnostic at this level
+        """
+    
+    def output(self, on=True):
+        """
+        All Generators must be able to output something, so therefore we need a method to turn the output on
+        """
+
+    
---
+++ src/piec/drivers/outline/outline.md
@@ -0,0 +1,13 @@
+The working tree for the outline of PIEC (beta version)
+
+1. Instrument
+2. Generator // Measurer (name WIP) // Other (stepper_motor, need a better name for this like indirect, bystander, secondary)
+3. AWG, RF_source, Pulser, Source_meter // Oscilloscope, Lockin, DAQ // Stepper_motor
+4. Individual models
+
+
+Note this is different than before:
+1. Instrument
+2. SCPI_instrument // Arduino // Digilent
+3. generic instruments
+4. specific instruments
