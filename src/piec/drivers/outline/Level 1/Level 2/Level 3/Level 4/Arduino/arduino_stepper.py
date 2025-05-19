176e4280a5cca88a78eeb9afb14da4c027b1b4c6
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2025-05-19 12:09:54 -0400

    Update arduino_stepper.py
    changed outline to actually be an outline with no code

--- src/piec/drivers/outline/Level 1/Level 2/Level 3/Level 4/Arduino/arduino_stepper.py
+++ src/piec/drivers/outline/Level 1/Level 2/Level 3/Level 4/Arduino/arduino_stepper.py
@@ -11,23 +11,21 @@ class Geos_Stepper(Stepper):
     This is the base level - instrument specific of the Arduino_Stepper used in our lab
     """
     def __init__(self, address):
-        rm = ResourceManager()
-        self.instrument = rm.open_resource(address, baud_rate=115200)
-        self.instrument.timeout = 20000 #20s
-        self.steps_per_revolution = 200 #default value, only change IFF change in hardware is also managed
+        """
+        Connects to the instrument by opening a ResourceManager and talking to it (use PiecManager)
+        Sets the instrument timeout and the steps_per_revolution to ensure (hardcode this value based on hardware specs)
+        """
 
     def idn(self):
-        line = self.instrument.query('0,0') #calls in builtin method to check if serial communication works
-        if "Complete" in line:
-            return "Custom Arduino_Stepper Object at {}".format(self.instrument.resource_name)
-                
-        else:
-            print("Timeout error occurred while waiting for the Arduino.")
-            return "Not connected"
+        """
+        Overwrites idn functionality to work with arduino
+        """
     
     def step(self, num_steps, direction):
         """
-        Steps the stepper motor 
+        Steps the stepper motor by sending a write string (via query) where the format is "{steps},{direction}"
+        where direction = 0 is for CW and 1 is for CCW
+        returns the current position after the stepper stops moving
         args:
             self (pyvisa.resources.asrl.not sure): Arduino
             num_steps (str/int): Desired step size, must be an integer value
@@ -35,30 +33,14 @@ class Geos_Stepper(Stepper):
         returns:
             current_position (int) The current position as read from the arduino
         """
-        answer = self.instrument.query("{},{}".format(num_steps, direction)) #specially formatted string for arduino code to work. See arduino code under src\piec\drivers\arduino\motor_control_serial_piec\motor_control_serial_piec.ino for more information
-        number = int(re.search(r'-?\d+', answer).group())
-                
-        if "Complete" in answer:
-            return number
-                
-        else: 
-            print("Did not complete task")
-
 
     def set_zero(self):
         """
-        Hardcoded command that sets the arduinos position tracker to zero
+        Hardcoded command that sets the arduinos position tracker to zero. Sends a direction = 9 to set the zero
         """
-        time.sleep(2) #ensure that arduino has time to recieve the data
-        self.step(0,9)
 
     def read_position(self):
         """
-        Reads the current position without stepping the stepper
+        Reads the current position without stepping the stepper by sending in zero steps
+        since the arduino already returns position with each step
         """
-        time.sleep(2) #ensure that arduino has time to recieve the data
-        position = self.step(0,0) #steps 0 steps, so doesnt change position
-        if position is not None:
-            return position
-        else:
-            print("Position unknown")
