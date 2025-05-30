8d651c9592a9809735edc3eb7478d7fba5b35062
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2025-05-30 13:18:24 -0400

    Level 2 completed
    Technically I think the output ON is better than using STAT but it is correct

--- src/piec/drivers/Level 1/Level 2/generator.py
+++ src/piec/drivers/Level 1/Level 2/generator.py
@@ -1,11 +1,9 @@
 """
-This is an outline for what the generator.py file should be like.
-
 A generator is defined as an instrument that generates something (e.g. a voltage/current)
 """
 from ..instrument import Instrument
+
 class Generator(Instrument):
-    # Initializer / Instance attributes
     """
     All generators must be able to output something
     """
@@ -13,10 +11,13 @@ class Generator(Instrument):
         """
         Opens the instrument and enables communication with it. Still communication method agnostic at this level
         """
+        super().__init__(address)
     
     def output(self, channel, on=True):
         """
         All Generators must be able to output something, so therefore we need a method to turn the output on.
         """
-
-    
+        state = "ON" if on else "OFF"
+        # Using a common SCPI format for controlling output state per channel.
+        # Assumes self.instrument.write is available from the Instrument base class.
+        self.instrument.write(f"OUTP{channel}:STAT {state}")
--- src/piec/drivers/Level 1/Level 2/secondary.py
+++ src/piec/drivers/Level 1/Level 2/secondary.py
@@ -1,11 +1,9 @@
 """
-This is an outline for what the secondary.py file should be like. NOTE: name is tbd
-
 A secondary is defined as an instrument that neither generates something nor measures something maybe peripheral (I like this one @alex thoughts?)
 """
 from ..instrument import Instrument
+
 class Secondary(Instrument):
-    # Initializer / Instance attributes
     """
     Note as a secondary, this need not have any extra functionality than the base instrument class since at this level
     we do not know what this instrument should do.
@@ -14,7 +12,5 @@ class Secondary(Instrument):
         """
         Opens the instrument and enables communication with it. Still communication method agnostic at this level
         """
-    
-    
-
-    
+        super().__init__(address)
+    # No additional methods are defined as per the class docstring.
