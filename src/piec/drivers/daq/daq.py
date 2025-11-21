daba6bd5f895fc423f38c8097761891ab1672bd3
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2025-11-21 15:53:08 -0500

    change daq to simpler version

--- src/piec/drivers/daq/daq.py
+++ src/piec/drivers/daq/daq.py
@@ -1,120 +1,115 @@
 """
-This is an outline for what the daq.py file should be like.
-
-A daq (Data Acqusition System) is defined as an instrument that has the typical features one expects a daq to have
+A DAQ (Data Acquisition System) is defined as an instrument capable of converting 
+analog physical signals to digital values (Input) and/or digital values to analog signals (Output).
 """
 from ..instrument import Instrument 
+
 class Daq(Instrument):
-    # Initializer / Instance attributes
-    """
-    All daqs must be able to acquire data and output signals. Need a way to get the list of analog and digital IO
-    """
-    #Core Information Functions
-    """
-    We should have some hardcoded information about the daq, such as the number of analog and digital channels, the sample rate, etc.
-    """
-    #analog input functions
-    def set_AI_channel(self, channel):
-        """
-        Sets the Analog input channel for data acquisition
-        """
-    def set_AI_range(self, channel, range):
-        """
-        Sets the range for the Analog input channel
-        """
-    def set_AI_sample_rate(self, channel, sample_rate):
-        """
-        Sets the sample rate for the Analog input channel
-        """
-    def configure_AI_channel(self, channel, range, sample_rate):
-        """
-        Calls the set_AI_channel, set_AI_range, and set_AI_sample_rate functions to configure the Analog input channel
-        """
+    # --- Class Attributes (Hardware Capabilities) ---
+    # Valid integer channel numbers. Empty list [] implies feature not supported.
+    ai_channels = [1] 
+    ao_channels = [1] 
+    dio_channels = [1]
     
-    #analog output functions
-    def set_AO_channel(self, channel):
-        """
-        Sets the Analog output channel for data output
-        """
-    def set_AO_range(self, channel, range):
-        """
-        Sets the range for the Analog output channel
-        """
-    def set_AO_sample_rate(self, channel, sample_rate):
-        """
-        Sets the sample rate for the Analog output channel
-        """
-    def configure_AO_channel(self, channel, range, sample_rate):
-        """
-        Calls the set_AO_channel, set_AO_range, and set_AO_sample_rate functions to configure the Analog output channel
-        """
-    #NOTE: Issue with digital IO is that some are only input and some only output but most can be both.
-    #digital input_only functions
-    def set_DI_channel(self, channel):
-        """
-        Sets the Digital input channel for data acquisition
-        """
-    def set_DI_sample_rate(self, channel, sample_rate):
-        """
-        Sets the sample rate for the Digital input channel
-        """
-    def configure_DI_channel(self, channel, sample_rate):
-        """
-        Calls the set_DI_channel and set_DI_sample_rate functions to configure the Digital input channel
-        """
-    #digital output_only functions
-    def set_DO_channel(self, channel):
-        """
-        Sets the Digital output channel for data output
-        """
-    def set_DO_sample_rate(self, channel, sample_rate):
-        """
-        Sets the sample rate for the Digital output channel
-        """
-    def configure_DO_channel(self, channel, sample_rate):
-        """
-        Calls the set_DO_channel and set_DO_sample_rate functions to configure the Digital output channel
-        """
-    #digital input/output functions
-    def set_DIO_channel(self, channel):
-        """
-        Sets the Digital input/output channel for data acquisition or output
-        """
-    def set_DIO_mode(self, channel, mode):
-        """
-        Sets the mode for the Digital input/output channel (input or output)
-        """
-    def set_DIO_sample_rate(self, channel, sample_rate):
-        """
-        Sets the sample rate for the Digital input/output channel
-        """
-    def configure_DIO_channel(self, channel, mode, sample_rate):
-        """
-        Calls the set_DIO_channel, set_DIO_mode, and set_DIO_sample_rate functions to configure the Digital input/output channel
-        """
-    #data acquisition functions
-    def quick_read(self):
-        """
-        Quick read function that returns the default data (off of whatever is the default channel typically 0 or 1) (e.g., analog input data)
-        """
-    def read_data(self, channel):
-        """
-        Reads the data from the specified channel (e.g., analog input, digital input, etc.)
-        Logic may be needed to determine the type of channel and read accordingly
-        """
-    def read_AI(self, channel):
-        """
-        Reads the Analog input data from the specified channel
-        """
-    def read_DI(self, channel):
-        """
-        Reads the Digital input data from the specified channel
-        """
-    #ouput functions
-    def output(self, channel, on=True):
-        """
-        Turns the output on or off for the specified channel (e.g., Analog output, Digital output
-        may require logic to determine the type of channel and output accordingly)
-        """
+    # Supported voltage ranges as (min, max) tuples. 
+    # Essential for scaling raw bits to real-world units.
+    ai_ranges = [(None, None)] 
+    ao_ranges = [(None, None)]
     
+    # Logic directions supported by digital ports
+    # 'IN' = Input, 'OUT' = Output, 'INOUT' = Configurable
+    dio_directions = ['I', 'O']
     
+    """
+    The Heart of the DAQ: Reading and Writing.
+    All DAQs must be able to perform immediate (software-paced) I/O.
+    """
+
+    # --- Analog I/O ---
+
+    def read_analog(self, channel):
+        """
+        Reads a single float value (voltage) from the specified Analog Input channel.
+        This is the fundamental 'Get Data' command.
+        args:
+            channel (int): The channel to read from
+        returns:
+            float: The measured value in Volts
+        """
+
+    def write_analog(self, channel, value):
+        """
+        Writes a single float value (voltage) to the specified Analog Output channel.
+        This is the fundamental 'Set Output' command.
+        args:
+            channel (int): The channel to write to
+            value (float): The voltage to output
+        """
+
+    def set_ai_range(self, channel, voltage_range):
+        """
+        Configures the gain/range for an Analog Input channel. 
+        Required because measurement resolution depends on this.
+        args:
+            channel (int): The channel to configure
+            voltage_range (tuple): The (min, max) range desired (e.g. (-10, 10))
+        """
+
+    def set_ao_range(self, channel, voltage_range):
+        """
+        Configures the output range for an Analog Output channel.
+        args:
+            channel (int): The channel to configure
+            voltage_range (tuple): The (min, max) range desired
+        """
+
+    # --- Digital I/O ---
+
+    def read_digital(self, channel):
+        """
+        Reads the state of a digital channel.
+        args:
+            channel (int): The channel to read
+        returns:
+            int: 1 (High) or 0 (Low)
+        """
+
+    def write_digital(self, channel, state):
+        """
+        Sets the state of a digital output channel.
+        args:
+            channel (int): The channel to write to
+            state (int/bool): 1/True for High, 0/False for Low
+        """
+
+    def set_dio_direction(self, channel, direction):
+        """
+        Configures the physics of the digital pin (Input vs Output).
+        Essential because writing to an Input pin can damage hardware.
+        args:
+            channel (int): The channel to configure
+            direction (str): 'IN' or 'OUT'
+        """
+
+    # --- Configuration Wrappers ---
+    
+    def configure_ai(self, channel, voltage_range=None):
+        """
+        Helper to configure an AI channel in one line.
+        """
+        if voltage_range is not None:
+            self.set_ai_range(channel, voltage_range)
+
+    def configure_ao(self, channel, voltage_range=None):
+        """
+        Helper to configure an AO channel in one line.
+        """
+        if voltage_range is not None:
+            self.set_ao_range(channel, voltage_range)
+
+    def configure_dio(self, channel, direction=None):
+        """
+        Helper to configure a DIO channel in one line.
+        """
+        if direction is not None:
+            self.set_dio_direction(channel, direction)
